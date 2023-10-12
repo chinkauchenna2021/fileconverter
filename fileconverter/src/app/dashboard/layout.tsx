87b9c648076057dd10fca021736d68b18796f156
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-10-12 05:07:41 -0700

    fileconverter first commit

---
+++ fileconverter/src/app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+// import { LayoutDashboard } from "lucide-react";
+
+import React, { ReactNode } from "react";
+
+export default function Layout({children}:{children:ReactNode}){
+   return(
+        <div>
+          {children}
+        </div>
+   )
+
+}
---
+++ fileconverter/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+      <div className='max-w-5xl m-auto' >
+            <Button size={"lg"}>Api page</Button>
+      </div>
+  )
+}

265e926962b007f070de3199b509a479282962d9
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-11-18 13:05:33 -0800

    fixed upload

--- src/app/api/filemanager/route.ts
+++ src/app/api/filemanager/route.ts
@@ -7,6 +7,7 @@ import type { IFiles, IStateContent } from "../../../../types/fileTypes";
 import { fetchFile, toBlobURL } from "@ffmpeg/util";
 import { ffmpegClient } from "@/libs/ffmpegClient";
 import { randomUUID } from "crypto";
+// import {upcast} from "upcast"
 const executeProcess = promisify(exec);
 
 interface IState {
@@ -20,13 +21,16 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   try {
     const fileRequest = await request.formData();
-    const allfileConversion = fileRequest.get("uploadedFileObject");
-    const jsonObject = JSON.parse(String(allfileConversion));
-    console.log(jsonObject.data);
+    const alldata =  await fileRequest.get("uploadedFileObject");
+    // const objJson = JSON.stringify(allfileConvert)
+    const jsonObject = await JSON.parse(String(alldata)) as IStateContent;
+    let fileBlob = String(jsonObject.file).split(',')[1];
+    let filePath = join("C:/Users/HP/Desktop/fileconvert/fileconverter/public/images/"+ randomUUID()+"."+jsonObject.originalExt)
+     await writeFile(filePath,fileBlob,"base64")
     // const  file:IFiles = fileRequest.get("uploadedFileObject") as unknown as File;
     // const fileConvertionextension = fileRequest.get('fileconversionext') as string ;
     // const fileconversionURLType  = fileRequest.get("fileconversionURLType") as string
-
+     console.log(filePath , __dirname)
     // if (!file) {
     //  return  NextResponse.json({ success: false });
     // }
@@ -34,8 +38,11 @@ export async function POST(request: NextRequest) {
     // const filedata = Buffer.from(bytes);
     // const convertURL = await ffmpegClient(file,filedata ,fileConvertionextension ,fileconversionURLType);
 
-    return NextResponse.json(jsonObject);
+    return NextResponse.json(fileBlob);
+
   } catch {
+
     return NextResponse.json({ response: "not correct" });
+
   }
 }
--- stores/useDropFile.ts
+++ stores/useDropFile.ts
@@ -4,6 +4,7 @@ import { persist } from "zustand/middleware";
 import { IStateContent } from "../types/fileTypes";
 import Axios from "axios";
 import { getFileToBase64 } from "@/hooks/getFileToBase64";
+// import { setInterval } from "timers/promises";
 
 interface State {
   droppedFiles: IStateContent[];
@@ -31,7 +32,6 @@ export const useDropFile = create<State>((set, get) => ({
     set(() => ({ droppedFiles: fileFormatChanged }));
   },
   fileSendToServer: async (allFileUpload: IStateContent[]) => {
-    const formData = new FormData();
 
     let newFile = allFileUpload?.map(async (item: IStateContent) => ({
       ...item,
@@ -39,22 +39,39 @@ export const useDropFile = create<State>((set, get) => ({
         .then((data) => data)
         .then((d) => d),
     }));
-    let datas = await Promise.all(newFile);
-    let dataObj = { data: datas };
-    formData.append("uploadedFileObject", JSON.stringify(dataObj));
-    let allConvertedFiles = await Axios({
-      method: "POST",
-      url: "http://localhost:3000/api/filemanager",
-      data: formData,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "multipart/form-data",
-      },
-      onUploadProgress: (progressEvent) => {
-        console.log(progressEvent.loaded);
-      },
-    });
-    //  console.log(allConvertedFiles.data)
+    
+    let filesWithBlob = await Promise.all(newFile);
+    filesWithBlob?.map((items , index)=>{
+      const formData = new FormData();
+       formData.append("uploadedFileObject" , JSON.stringify(items))
+       setTimeout(async ()=>{
+         let allConvertedFiles = await Axios({
+           method: "POST",
+           url: "http://localhost:3000/api/filemanager",
+           data: formData,
+           headers: {
+             "Accept": "application/json",
+             "Content-Type": "multipart/form-data",
+           },
+     
+         });
+         console.log(allConvertedFiles)
+       },3000)
+     })
+
+    // let dataObj = { data: datas };
+    // formData.append("uploadedFileObject", JSON.stringify(dataObj));
+    // let allConvertedFiles = await Axios({
+    //   method: "POST",
+    //   url: "http://localhost:3000/api/filemanager",
+    //   data: formData,
+    //   headers: {
+    //     "Accept": "application/json",
+    //     "Content-Type": "multipart/form-data",
+    //   },
+
+    // });
+    //  console.log(allConvertedFiles)
     // set(()=>({convertedFiles:allConvertedFiles}))
   },
 }));

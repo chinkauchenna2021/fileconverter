cd259ad8ceab09793ef50a6d2952904f46cd7da4
chinkauchenna2021 <uchennachinka4@gmail.com>
2023-11-21 03:12:37 -0800

    add main

--- src/app/api/filemanager/route.ts
+++ src/app/api/filemanager/route.ts
@@ -1,30 +1,71 @@
-import { writeFile, readFile, readdir, unlink } from "fs/promises";
+import { writeFile, readFile, readdir, unlink} from "fs/promises";
 import { promisify } from "util";
 import { exec } from "child_process";
 import { NextRequest, NextResponse } from "next/server";
-import {join , extname , dirname} from "path";
-import type { IFiles } from "../../../../types/fileTypes";
-import { fetchFile, toBlobURL } from '@ffmpeg/util'
-import { ffmpegClient } from "@/libs/ffmpegClient";
+import { join, extname, dirname } from "path";
+import type { IFiles, IStateContent } from "../../../../types/fileTypes";
+import { FfmpegClient } from "../../../libs/ffmpegClient";
 import { randomUUID } from "crypto";
+import { object } from "zod";
+
+// import {upcast} from "upcast"
 const executeProcess = promisify(exec);
 
-export async function GET(request:NextRequest){
-  return NextResponse.json({success:true});
+interface IState {
+  data: IStateContent[];
+}
+
+export async function GET(request: NextRequest) {
+  return NextResponse.json({ success: true });
 }
 
 export async function POST(request: NextRequest) {
-  const fileRequest = await request.formData();
-  const  file:IFiles = fileRequest.get("file") as unknown as File;
-  const fileConvertionextension = fileRequest.get('fileconversionext') as string ;
-  const fileconversionURLType  = fileRequest.get("fileconversionURLType") as string
+  try {
+    const fileRequest = await request.formData();
+    const alldata =  await fileRequest.get("uploadedFileObject");
+    const jsonObject = await JSON.parse(String(alldata)) as IStateContent;
+    let fileBlob =  String(jsonObject.file).split(',')[1];
+    let fileName =  randomUUID()+"."+jsonObject.originalExt ;
+    const fileConverionType = jsonObject.fileConversionFormat;
+    const mimeType = jsonObject.fileType+"/"+fileConverionType;
+    let filePath = join("C:/Users/HP/Desktop/fileconvert/fileconverter/public/images/"+ fileName)
+    let outputFileName = randomUUID()+"."+jsonObject.fileConversionFormat;
+    let newFilePath = join("C:/Users/HP/Desktop/fileconvert/fileconverter/public/images/"+ outputFileName)
 
-  if (!file) {
-   return  NextResponse.json({ success: false });
+    await writeFile(filePath,fileBlob,"base64");
+
+    await FfmpegClient(filePath , fileName ,newFilePath ,outputFileName)
+  //  const returndata =  await ffmpeg.exec(['-i',fileName, outputPath])
+  //   const data = await ffmpeg.readFile('readFile', outputPath)
+    const returndata = "nice"
+    
+    //  let baseFile =  base64ToFile(jsonObject.file as any);
+    // let url = URL.createObjectURL(fileBlob as any)
+    // console.log(baseFile)
+    // let blobFile =   await uploadImage(jsonObject.file as any , jsonObject.filename as string ) as unknown  as File
+    //  const newfile =  urltoFile(String(jsonObject.file) , jsonObject.file as any , jsonObject.fileType) ;
+    //  await writeFile(filePath,fileBlob,"base64");
+    //  console.log(blobFile)
+    // const  file:IFiles = fileRequest.get("upload edFileObject") as unknown as File;
+    // const fileConvertionextension = fileRequest.get('fileconversionext') as string ;
+    // const fileconversionURLType  = fileRequest.get("fileconversionURLType") as string
+    // const filesObject = new File([fileBlob] , "image" ,{type:jsonObject.fileType})
+    // const newFile = await blobToFile(fileBlob,fileName)
+    //  console.log(filePath , newFile)
+    // if (!file) {
+    //  return  NextResponse.json({ success: false });
+    // }
+    // const bytes = await file?.arrayBuffer();
+    // const filedata = Buffer.from(bytes);
+    // fileName: string,
+    // filedata:  Blob,
+    // conversionexttype: FileconversionType,
+    // convesionURLType: FileURLType
+    // const convertURL = await ffmpegClient(jsonObject.filename as string,fileBlob as any ,jsonObject.fileConversionFormat ,jsonObject.fileType);
+    return NextResponse.json({returndata});
+  } catch {
+
+    return NextResponse.json({ response: "not correct" });
+
   }
-  const bytes = await file?.arrayBuffer();
-  const filedata = Buffer.from(bytes);
-
-  const convertURL = await ffmpegClient(file,filedata ,fileConvertionextension ,fileconversionURLType);
- return  NextResponse.json({success:true , mssage:"open file to check for your image"});
 }
--- src/libs/ffmpegClient.ts
+++ src/libs/ffmpegClient.ts
@@ -1,56 +1,31 @@
+"use client"
+
 import { writeFile, readFile, readdir, unlink } from "fs/promises";
-import { FFmpeg } from '@ffmpeg/ffmpeg'
-import {join , extname , dirname} from "path";
-import { fetchFile, toBlobURL } from '@ffmpeg/util'
-import { useRef, useState } from 'react'
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { join, extname, dirname } from "path";
+import { useRef, useState , useEffect } from "react";
 import { randomUUID } from "crypto";
 import prisma from "./prismaClientGlobal";
-import { getSessionIfNotNull } from "./auth";
+// import { getSessionIfNotNull } from "./auth";
 import { getFilename } from "@/hooks/getFilename";
 
-type  Ifiles = File | null;
-type Filedata =  Buffer 
-type FileconversionType = string
-type FileURLType = string
 
+type Ifiles = File | null;
+type Filedata = Buffer;
+type FileconversionType = string;
+type FileURLType = string;
 
-export const ffmpegClient = async (file:Ifiles, filedata:Filedata ,conversionexttype:FileconversionType , convesionURLType:FileURLType) =>{
-    const [loaded, setLoaded] = useState(false)
-    const [isLoading, setIsLoading] = useState(false)
-    const ffmpegRef = useRef(new FFmpeg())
-    const messageRef = useRef<HTMLParagraphElement | null>(null)
-    const session = getSessionIfNotNull();
-    
-    const ffmpeg = ffmpegRef.current 
-    ffmpeg.on('log', ({ message }) => {
-        if (messageRef.current) messageRef.current.innerHTML = message
-      })
+export const FfmpegClient = async (originalFileDir:string , originalFileName:string ,conversionFiledir:string , fileconversionName:string) => {
   
-    await ffmpeg.load({
-        coreURL: await toBlobURL(`${String(process.env.FFMPEG_BASEURL)}/ffmpeg-core.js`, 'text/javascript'),
-        wasmURL: await toBlobURL(`${String(process.env.FFMPEG_BASEURL)}/ffmpeg-core.wasm`, 'application/wasm')
-      });
-      let filename = getFilename(file?.name as string);
-      let  randName =  filename+"_"+randomUUID();
-      let filePath =  join('/public/input/'+ file?.name);
-      let newFileExt =  randName+"."+conversionexttype;
-      await writeFile(filePath , filedata);
-       // await ffmpeg.writeFile('input.avi', await fetchFile('video-15s.avi'))
-       await ffmpeg.exec(['-i', filePath, newFileExt])
-       const data = (await ffmpeg.readFile(newFileExt)) as any
-       let convertURL = URL.createObjectURL(new Blob([data.buffer], { type: convesionURLType }))
-        if(convertURL && session?.user?.email != null ){
-           prisma.user.findUnique({
-            where:{
-               email:session.user?.email
-            },
-            select:{
+  
+  (async(originalFileName ,fileconversionName)=>{
+      const ffmpeg = createFFmpeg({ log: true });
+      await ffmpeg.load();
+      ffmpeg.FS('writeFile',originalFileName, await fetchFile(originalFileDir));
+      await ffmpeg.run('-i', originalFileName, fileconversionName);
+      await writeFile(conversionFiledir, ffmpeg.FS('readFile', fileconversionName));
+    })(originalFileName ,fileconversionName)
+ 
 
-            }
-           })
-
-
-        
-        }
-        return convertURL;
-}
+  }
